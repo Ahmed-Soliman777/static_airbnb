14398d10f7900be247314d5fab5804ad4c7b2386
Abdelmajeed Abdelfattah <megoo2810@gmail.com>
2025-09-23 13:19:51 +0300

    create experience page

---
+++ src/Data/experience.js
@@ -0,0 +1,253 @@
+export const original = [
+  {
+    id: 1,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-6201447/original/6b80385e-18e3-4889-b0e8-75784bfbc1d5.jpeg?im_w=480",
+    title: "Lunch with fashion icon Lenny Niemeyer in her home",
+    country: "Rio de Janeiro,Brazil",
+    cost: "From £80 / guest",
+    rate: 4.5,
+  },
+  {
+    id: 2,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-6146467/original/a752fe02-1c64-48d2-be3c-b469723ee0e8.jpeg?im_w=480",
+    title: "Hit the ice with Paralympian Andrea Macrì",
+    country: "Turin,Italy",
+    cost: "From £23 / guest",
+    rate: 4.5,
+  },
+  {
+    id: 3,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-6259920/original/ed0abd96-d2e6-4bd1-b549-08dae16eb858.jpeg?im_w=480",
+    title: "Create seasonal ikebana with Watarai Toru",
+    country: "KamaKura,Japan",
+    cost: "From £72 / guest",
+    rate: 5.0,
+  },
+  {
+    id: 4,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-6259774/original/df98a1fc-b96c-4aed-90bd-f8d3649dc512.jpeg?im_w=480",
+    title: "Dine in a live art show at the Palazzo Castiglioni",
+    country: "Milan,Italy",
+    cost: "From £76 / guest",
+    rate: 4.5,
+  },
+  {
+    id: 5,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-6170242/original/8a9320c7-348b-498b-918c-b6f08a31a82f.jpeg?im_w=480",
+    title: "Dine, drink, and dance with Chef Thomas Troisgros",
+    country: "Rio de Janeiro,Brazil",
+    cost: "From £92 / guest",
+    rate: 5.0,
+  },
+  {
+    id: 6,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-5975701/original/1536d75f-932a-459d-bcfa-c43d8bc13660.jpeg?im_w=480",
+    title: "Carve marble with a third-generation sculptor",
+    country: "Athens,Greece",
+    cost: "From £44 / guest",
+    rate: 5.0,
+  },
+  {
+    id: 7,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-5946904/original/3fc78212-954c-4e94-8464-1513c5a8cd91.jpeg?im_w=480",
+    title: "Experience a sacred Buddhist ritual and yoga class",
+    country: "Haiya Sub-district,Thailand",
+    cost: "From £9 / guest",
+    rate: 5.0,
+  },
+];
+export const cairo = [
+  {
+    id: 1,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-1967445/original/e0b657c9-fd0d-4b57-9fc3-04ed3a263c6d.jpeg?im_w=480",
+    title: "Private tour to Giza Pyramids, Sphinx, ATVs& Lunch",
+    cost: "From £48 / guest",
+    rate: 4.93,
+    num: "09",
+  },
+  {
+    id: 2,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4406448/original/88662094-efe3-4613-966f-caa30d2fa2aa.jpeg?im_w=480",
+    title: "Local Cairo Walking & Food Tour",
+    cost: "From £29 / guest",
+    rate: 4.94,
+    num: "18",
+  },
+  {
+    id: 3,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-3141917/original/d0d8f43c-76f3-4ccb-825a-7f6c207e21ef.jpeg?im_w=480",
+    title: "Engage with Egyptian life & Bazzar",
+    cost: "From £26 / guest",
+    rate: 4.97,
+    num: "19",
+  },
+  {
+    id: 4,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4287589/original/45543c5a-8640-4056-804e-6ec70b51ca0f.jpeg?im_w=480",
+    title: "Old Cairo, Coptic, Islamic & Khan El Khalili Bazar",
+    cost: "From £37 / guest",
+    rate: 4.9,
+    num: "09",
+  },
+  {
+    id: 5,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-3747217/original/e67f288d-ecf7-4407-9c75-9117fb56c603.jpeg?im_w=480",
+    title: "Sail the Nile at sunset-A Stunning felucca",
+    cost: "From £22 / guest",
+    rate: 4.95,
+    num: "06",
+  },
+  {
+    id: 6,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4666579/original/306887d5-d049-426d-83e9-9ef06ec8f3cd.jpeg?im_w=480",
+    title: "Private tour to Giza Pyramids, Sphinx, ATVs& lunch",
+    cost: "From £48 / guest",
+    rate: 4.94,
+    num: "08:30",
+  },
+  {
+    id: 7,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-381859/original/d144a4b1-ce15-4a09-b1af-872074b31944.jpeg?im_w=480",
+    title: "Enjoy an evening dinner Nile cruise",
+    cost: "From £26 / guest",
+    rate: 4.79,
+    num: "19",
+  },
+];
+export const weekEnd = [
+  {
+    id: 1,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-987375/original/faac2cbc-0e23-4c04-80e2-1f13b9a01e6a.jpeg?im_w=960",
+    title: "Full day, Giza Pyramids, Sphinx, Saqqara, Memphis",
+    cost: "From £30 / guest",
+    rate: 4.93,
+    num: "Fri  08",
+  },
+  {
+    id: 2,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4706113/original/b405bfa8-e0c9-460b-abeb-3e872d27b142.jpeg?im_w=480",
+    title: "The Grand Egyptian Museum, Giza Pyramids & Sphinx",
+    cost: "From £36 / guest",
+    rate: 4.88,
+    num: "Fri  07",
+  },
+  {
+    id: 3,
+    image:
+      "https://a0.muscache.com/im/pictures/lombard/MtTemplate-3305299-active_media/original/3cc1a31f-68d6-4ed3-b9d6-bd3d78e2f0a2.jpg?im_w=480",
+    title: "Explore Giza and Saqqara",
+    cost: "From £37 / guest",
+    rate: 4.95,
+    num: "Fri  08:30",
+  },
+  {
+    id: 4,
+    image:
+      "https://a0.muscache.com/im/pictures/lombard/MtTemplate-3509047-active_media/original/e9c4534f-c186-4364-8b60-d7f571e18785.jpg?im_w=480",
+    title: "Cairo Street Food with a Local Family",
+    cost: "From £27 / guest",
+    rate: 4.98,
+    num: "Fri  14:30",
+  },
+  {
+    id: 5,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-1372754/original/826a636f-7dcb-4d91-adc7-5a7eaccb3a91.jpeg?im_w=480",
+    title: "Photographic journey through the Pyramids",
+    cost: "From £37 / guest",
+    rate: 4.89,
+    num: "Fri  09",
+  },
+  {
+    id: 6,
+    image:
+      "https://a0.muscache.com/im/pictures/lombard/MtTemplate-1093446-active_media/original/6ce79308-059e-483d-a44a-3d15ad3de703.jpg?im_w=480",
+    title: "Navigate The Pyramids and Saqqara",
+    cost: "From £37 / guest",
+    rate: 4.99,
+    num: "Fri  08:30",
+  },
+  {
+    id: 7,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-1062432/original/27b91861-c64d-4382-9f1e-a5db60a4c11f.jpeg?im_w=480",
+    title: "Guided tour: Pyramids, Sphinx, Saqqara& Memphis",
+    cost: "From £30 / guest",
+    rate: 4.99,
+    num: "Fri  08",
+  },
+];
+export const PopularCairo = [
+  {
+    id: 1,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-381859/original/d144a4b1-ce15-4a09-b1af-872074b31944.jpeg?im_w=480",
+    title: "Enjoy an evening dinner Nile cruise",
+    cost: "From £26 / guest",
+    rate: 4.79,
+  },
+  {
+    id: 2,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4703090/original/9796c8c3-109e-41e4-929f-791676c9ef77.jpeg?im_w=960",
+    title: "Try hands-on pottery class with crafts specialist",
+    cost: "From £33 / guest",
+    rate: 4.93,
+  },
+  {
+    id: 3,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-1062432/original/27b91861-c64d-4382-9f1e-a5db60a4c11f.jpeg?im_w=480",
+    title: "Guided tour: Pyramids, Sphinx, Saqqara& Memphis",
+    cost: "From £30 / guest",
+    rate: 4.91,
+  },
+  {
+    id: 4,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-4367253/original/c9c8f441-6f4c-48cb-ae5f-a090f689c882.jpeg?im_w=480",
+    title: "Half day tour to great pyramids and sphinx",
+    cost: "From £26 / guest",
+    rate: 4.96,
+  },
+  {
+    id: 5,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-460971/original/8584ad6d-bf16-4a72-8394-06e87ad68106.jpeg?im_w=480",
+    title: "Cairo cooking class with an Egyptian family",
+    cost: "From £29 / guest",
+    rate: 4.98,
+  },
+  {
+    id: 6,
+    image:
+      "https://a0.muscache.com/im/pictures/lombard/MtTemplate-4648480-active_media/original/29979612-69f4-4736-a660-9b3c633efd9c.jpg?im_w=480",
+    title: "Egyptian Coffee Tasting & Local Breakfast",
+    cost: "From £16 / guest",
+    rate: 4.98,
+  },
+  {
+    id: 7,
+    image:
+      "https://a0.muscache.com/im/pictures/Mt/MtTemplate-1502691/original/c8e41e3a-5836-406e-8179-0c50d009f2d9.jpeg?im_w=960",
+    title: "PrivateTour to Giza Pyramids Grand Egyptian Museum",
+    cost: "From £37 / guest",
+    rate: 4.89,
+  },
+];
