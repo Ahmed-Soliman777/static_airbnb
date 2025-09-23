680c6a38f43bd36cf9f74850e30657337a952db7
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2025-09-23 21:45:25 +0300

    Add renames files

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from '../header/header'
+import Header from '../header/Header'
 import Footer from '../footer'
 
 export default function Layout() {
